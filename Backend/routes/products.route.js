1a079fe899bf1f7bfa9e2a3b948925611e64cda9
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-19 03:08:14 +0530

    removed spaces

--- Backend/routes/products.route.js
+++ Backend/routes/products.route.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const controller  = require('../controllers/products.controller')
-const productRoute = express.Router()
+const express = require("express");
+const controller = require("../controllers/products.controller");
+const productRoute = express.Router();
 
-productRoute.get('/' , controller.getall)
-productRoute.get('/:id' , controller.getall)
+productRoute.get("/", controller.getall);
+productRoute.get("/:id", controller.getall);
+productRoute.post("/add", controller.create);
+productRoute.post("/addAllDuckingData", controller.addAllDuckingData);
+productRoute.patch("/update/:id", controller.update);
+productRoute.delete("/delete/:id", controller.delete);
 
-productRoute.post('/create', controller.create)
-
-productRoute.post('/addAllDuckingData', controller.addAllDuckingData)
-
-productRoute.patch('/update/:id', controller.update)
-productRoute.delete('/delete/:id', controller.delete)
-
-
-module.exports = productRoute
+module.exports = productRoute;
