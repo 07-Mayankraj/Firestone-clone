e5943d80aeb41b34383581fbfc21b8ab1aab9724
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-18 19:41:19 +0530

    user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name : String,
+    email : String,
+    gender : String,
+    password : String
+})
+
+const UserModel = mongoose.model('user',userSchema)
+
+module.exports = UserModel;
