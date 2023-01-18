abd59d5809eeb3a0cf8f8574e026dfc957e5be49
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-19 02:40:51 +0530

    gender removed and phone added to user schema

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name : String,
     email : String,
-    gender : String,
+    phone : Number,
     password : String
 })
 
