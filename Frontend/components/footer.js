279c358044e2c3fe817e62045f8d32b035905fc2
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-20 17:25:08 +0530

    footer compo

---
+++ Frontend/components/footer.js
@@ -0,0 +1,111 @@
+let footer = () => {
+  return `
+  <div>
+  <hr class="footer-hr footer-hr-top" />
+
+  <div id="end">
+    <div id="size">
+      <ul>
+        <h5>TIRES</h5>
+        <li>Alenza</li>
+        <li>Blizzak</li>
+        <li>Turanza</li>
+        <li>Ecopia</li>
+        <li>WeatherGrip</li>
+        <li>Winterforce</li>
+        <li>DriveGuard</li>
+        <li>WeatherPeak</li>
+        <li>Destination</li>
+        <li>Firestone All Season</li>
+        <li>Firestone</li>
+        <li>Dueler</li>
+        <li>Run-Flat</li>
+        <li>Firehawk</li>
+        <li>SureDrive</li>
+        <li>Toyo</li>
+
+        <h5>ABOUT TIRES</h5>
+        <li>Firestone Racing</li>
+        <li>Tire Recall Information</li>
+        <li>Tire Warranties</li>
+        <li>Tire Tread Depth</li>
+        <li>Shop Tires</li>
+        <li>The Tire Advisor</li>
+        <li>Tires for SUVs & CUVs</li>
+        <li>Winter Tires</li>
+      </ul>
+    </div>
+
+    <div id="size">
+      <ul>
+        <h5>AUTO REPAIR</h5>
+        <li>Alignment</li>
+        <li>Brakes</li>
+        <li>Steering and Suspension Repair</li>
+        <li>Engine Repair Services</li>
+        <li>Tire Repair</li>
+        <li>Services</li>
+        <li>Tire Pressure Monitoring System (TPMS)</li>
+        <h5>AUTO MAINTENANCE</h5>
+        <li>Oil</li>
+        <li>Batteries</li>
+        <li>Tune-Up</li>
+        <li>Transmission</li>
+        <li>AC Service</li>
+        <li>Radiator</li>
+        <li>Drivetrain</li>
+        <li>Free Vehicle Inspections</li>
+        <li>Scheduled Maintenance</li>
+        <li>Electric & Hybrid Vehicles</li>
+      </ul>
+    </div>
+    <div id="size">
+      <ul>
+        <h5>FIRESTONE</h5>
+        <li>About Us</li>
+        <li>Firestone Credit Card</li>
+        <li>Boys and Girls Clubs of America</li>
+        <li>Careers</li>
+        <li>RESOLV</li>
+        <li>Firestone Complete Fleetcare</li>
+        <li>Firestone Direct Service Areas</li>
+        <li>Firestone Direct</li>
+        <li>Gasonomics</li>
+        <li>Gift Cards</li>
+        <li>Contact Us</li>
+        <li>Find a Store</li>
+        <li>Roadside Assistance Near Me</li>
+        <li>Schedule Appointment</li>
+        <li>Offers & Rebates</li>
+        <li>The Firestone Triple Promise</li>
+        <li>Vehicle Care</li>
+        <li>Wrench</li>
+      </ul>
+    </div>
+    <div id="size">
+      <ul>
+        <h5>ARTICLES</h5>
+        <li>Blog Home</li>
+        <li>Tires</li>
+        <li>Brakes</li>
+        <li>Batteries</li>
+        <li>Alignment</li>
+        <li>Oil Change</li>
+        <li>Maintenance</li>
+        <li>Driving</li>
+        <li>Videos</li>
+        <h5>WARRANTIES</h5>
+        <li>Battery Warranty Options</li>
+        <li>Service Warranty Options</li>
+        <li>Tire Warranties</li>
+      </ul>
+    </div>
+    <div>
+        <img width="80%" style="margin: 20px;" src="./resources/socialicons.png" alt="">
+    </div>
+  </div>
+</div>
+
+  `};
+
+export { footer };
