5104c4727ddbc30f7f633262d3d4c8d4e45d59a5
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-21 21:30:56 +0530

    fix loading animation

--- Frontend/components/loading.components.js
+++ Frontend/components/loading.components.js
@@ -2,22 +2,12 @@ let popHandler = document.querySelector(".pop-handler");
 
 function loading (){
   popHandler.innerHTML = `
-  <div style="width: 100%;position: fixed;top: 20%; left : 40% ; right : 40%;height: 100vh;backdrop-filter: blur(5px)" class="notification-popup" id="quickbuy-popup">
-  <lord-icon class="notification-loding" src="https://cdn.lordicon.com/kvsszuvz.json" trigger="loop"
-  colors="primary:#121331,secondary:#08a88a" style="width:450px;height:450px">
+  <div style="width: 100%;position: absolute;top: -10%;height: 100vh;backdrop-filter: blur(5px);display: flex;justify-content: center;align-items: center;" class="notification-popup" id="quickbuy-popup">
+  <lord-icon class="notification-loding" src="https://cdn.lordicon.com/kvsszuvz.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:50%;height:50%;">
   </lord-icon>
   </div>
   `
 }
-// function loading (){
-//   popHandler.innerHTML = `
-//   <div style="width: 100%;position: fixed;top: 0%;height: 100vh;backdrop-filter: blur(5px)" class="notification-popup" id="quickbuy-popup">
-//   <lord-icon class="notification-loding" src="https://cdn.lordicon.com/kvsszuvz.json" trigger="loop"
-//   colors="primary:#121331,secondary:#08a88a" style="width:450px;height:450px">
-//   </lord-icon>
-//   </div>
-//   `
-// }
 export default loading
 
 
