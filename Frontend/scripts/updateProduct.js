3abb543f33cc78187a4f2f88c4b91c80a4bfda5f
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-21 21:31:55 +0530

    update product page

--- Frontend/scripts/updateProduct.js
+++ Frontend/scripts/updateProduct.js
@@ -4,110 +4,9 @@ const baseURL = "https://rich-ruby-kitten-toga.cyclic.app";
 // fetching html tags
 let popHandler = document.querySelector(".pop-handler");
 
-loading();
-
-
-
 // card data
 
 //
-const data = await getProduct(localStorage.getItem("quick-data"))
-
-console.log(data);
-function getCard(data) {
-  let formatedData = data.map((ele) => {
-    return `
-                <div class="product-cards" id ="${ele._id}" >
-                
-                <div class="product-logo-div">
-                    <img class="pimage" src=${ele.brand_img} 
-                        alt="${ele.product_name}">
-                </div>
-                
-                <div class="product-image-div">
-                    <img class="pimage" src=${ele.product_img} 
-                        alt="${ele.product_name}">
-                </div>
-                <div class=" product-title">
-                    <p>${ele.product_name}</p>
-                </div>
-                <div class="product-price">
-                    <span class="product-price">${ele.review_count}</span>
-                </div>
-                <div class="product-button">
-                <button type="button" id ="${ele._id}" class="btn edit-btn  btn-warning btn-block">Edit</button>
-                <button type="button" id ="${ele._id}" class="btn delete-btn btn-danger btn-block">Delete</button>
-                </div>
-                <div class="product-reviews">
-                        <p>⭐⭐⭐⭐&#160${ele.review_count}</p>
-                </div>
-            </div>
-
-
-        `;
-  });
-  return formatedData.join("");
-}
-
-//  render the data
-
-async function renderData(product_data) {
-  let datadisplay = document.querySelector(".product-data-display");
-  datadisplay.innerHTML = getCard(product_data);
-
-  // delete button
-
-  let alldeletebtn = document.querySelectorAll(".delete-btn");
-  for (let btn of alldeletebtn) {
-    btn.addEventListener("click", (event) => {
-      let product_id = event.target.id;
-      loading();
-      deleteproduct(product_id);
-    });
-  }
-  
-  // edit 
-
-  let editbtnall = document.querySelectorAll(".edit-btn");
-  for (let btn of editbtnall) {
-    btn.addEventListener("click", async(event) => {
-      let product_id = event.target.id;
-      loading();
-      localStorage.setItem("quick-data", product_id);
-      hideLoading()
-      window.location.href = "../routes/editProduct.html"
-      
-    });
-  }
-
-
-  
-
-
-
-
-}
-// delete product
-async function deleteproduct(product_id){
-
-  try {
-    let product_res = await fetch(`${baseURL}/products/delete/${product_id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "applicaton/json",
-      },
-    });
-    if (product_res.ok) {
-      alert("product deleted successfully");
-      fetchData()
-    } else {
-      console.log(product_res);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 let getProduct = async (id) => {
   try {
     let product_res = await fetch(`${baseURL}/products/${id}`, {
@@ -127,8 +26,63 @@ let getProduct = async (id) => {
   }
 };
 
+const data = await getProduct(localStorage.getItem("quick-data"));
+renderData(data);
+console.log(data);
+
+function getCard(data) {
+  let formatedData = data.map((ele) => {
+    return `
+                <div class="product-cards" id ="${ele._id}" >
+                
+                <div class="product-logo-div">
+                    <img class="dimage" src=${ele.brand_img} 
+                        alt="${ele.product_name}">
+                </div>
+                
+                <div class="product-image-div">
+                    <img class="dimage" src=${ele.product_img} 
+                        alt="${ele.product_name}">
+                </div>
+                <div class=" product-title">
+                    <p><span class="titeloflist">Product Name : </span>${ele.product_name}</p>
+                </div>
+                <div class=" product-title">
+                    <p><span class="titeloflist">Rating : </span>${ele.average_rating}</p>
+                </div>
+                <div class=" product-title">
+                    <p><span class="titeloflist">Product Specs : </span>${ele.product_specs}</p>
+                </div>
+                <div class=" product-title">
+                    <p>${ele.product_specs}</p>
+                    <p>${ele.traction}</p>
+                    <p>${ele.noise_level}</p>
+                    <p>${ele.wet_traction}</p>
+                    <p>${ele.tire_wear}</p>
+                    <p>${ele.ride_comfort}</p>
+                    <p>${ele.dry_traction}</p>
+                </div>
+
+                
+                <div class="product-reviews">
+                        <p>⭐⭐⭐⭐&#160${ele.review_count}</p>
+                </div>
+            </div>
+
+
+        `;
+  });
+  return formatedData.join("");
+}
+
+//  render the data
+
+async function renderData(product_data) {
+  let datadisplay = document.querySelector(".product-details");
+  datadisplay.innerHTML = getCard(product_data);
+}
 
 //  hide laoding animations
-function hideLoading(){
-  popHandler.innerHTML = null
+function hideLoading() {
+  popHandler.innerHTML = null;
 }
