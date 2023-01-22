ddeb3e12fa8f07b45581624fc119fc6c907b8005
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-22 18:35:36 +0530

    backend pass fix

--- Backend/controllers/users.controller.js
+++ Backend/controllers/users.controller.js
@@ -36,13 +36,16 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await UserModel.find({ email })
+        console.log(user);
         if (user.length !== 0) {
-            await bcrypt.compare(password, user[0].password, (err, res)=>{
-                if(res){
+            await bcrypt.compare(password, user[0].password, (err, success)=>{
+                if(success){
                     token = jwt.sign({ userID: user[0]._id }, JWT_SECRET_KEY, { expiresIn: '24h' })
                     // token sent
-                    res.send({ "message": "login success", "Token" : `${token}` })
+                    res.json({ "message": "login success", "Token" : `${token}` })
                 } else {
+
+
                     res.status(403).send({ "message": "Invalid Credentials"})
                 }
             })
