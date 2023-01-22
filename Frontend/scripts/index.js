f650c33c65099a379f2e8ae4655868cf2a27d76a
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-22 16:21:03 +0530

    navbar appended

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -1,6 +1,10 @@
 import { footer } from "../components/footer.js";
+import navbar from "../components/navbar.components.js";
+
 
 console.log(footer());
 // footer 
 let footerDiv = document.querySelector('.footer');
+// let naverbarDiv = document.querySelector('#main-navbar')
+naverbarDiv.innerHTML = navbar()
 footerDiv.innerHTML = footer()
