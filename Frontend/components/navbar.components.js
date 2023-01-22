1b33a132271f51e88cebeeda89275049e4e134d3
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-01-22 18:59:54 +0530

    lgin signup navbar done

--- Frontend/components/navbar.components.js
+++ Frontend/components/navbar.components.js
@@ -1,255 +1,123 @@
 const navbar = () => {
-    return `<header>
-    <nav class="navbar">
-        <div class="nav-logo">
-            <img src="/logo/" alt="logo">
-        </div>
-        <div class="heading-links">
-            <ul class="heading-links-list">
-                <li>
-                    <a href="">Product</a>
-                    <div class="mega-menu">
-                        <div class="mega-menu-items">
-                            <div id="mega-menu-uls">
-                                <h4> Subscription Management ></h4>
-                                <ul>
+ return `  <div id="navbar" class="">
+ <div class=" top-heading">
+   <div class="black-heading top-heading-left">
+     <p >CAR CARE IS EVEN MORE COMPLETE WITH MY FIRESTONE APP</p >
+   </div>
+   <div class="white-heading top-heading-right">
+     <p >KEEP TRACT AND MAINTENANCE</p >
+   </div>
+ </div>
+ <div class="right-buttons">
+   <div></div>
+   <div class="right-buttons-links">
+     <a href="#">Blog </a>
+     <a href="#">Schedule an Appointment</a>
+     <a href="#">Contact Us</a>
+     <a href="#">Create an Account</a>
+     <a href="../routes/Login.html">Sign in</a>
+   </div>
+ </div>
+ <div id="main-navbar" class="">
+   <header>
+     <nav class="navbar">
+         <div class="nav-logo">
+             <img src="/logo/firewheel.png" alt="logo">
+         </div>
+         <div class="heading-links">
+             <ul class="heading-links-list">
+                 <li>
+                     <a href="">TIRE</a>
+                     <div class="mega-menu megamenutire">
+                         <div class="mega-menu-items ">
+                             <div class="mega-menu-uls">
+                                 <ul>
 
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Overview</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Create & Manage Plans</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Entitlements</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Pricing Management</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Handle Trial Subscriptions</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Customer Self-service Portal</a></li>
-                                    <li> <a
-                                            href="https://www.chargebee.com/subscription-management/?ref=navbar">
-                                            Mobile Solutions</a></li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h4>Billing Automation ></h4>
-                                <ul>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Billing Automation</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Overview</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Usage Based Billing</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Invoicing Automation</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Quotes</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/recurring-billing-invoicing/?ref=navbar">
-                                            Proration</a></li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h4>Recurring Payments ></h4>
-                                <ul>
+                                     <li> <a
+                                             href="./routes/product.html">
+                                             Shop Tires by Vehicle</a></li>
+                                     <li> <a
+                                             href="./routes/product.html">
+                                             Shop Tires by Size</a></li>
+                                     <li> <a
+                                             href="./routes/product.html">
+                                             Shop with Tire Decision Guide</a></li>
+                                     <li> <a
+                                             href="./routes/product.html">
+                                             Shop Tires</a></li>
+                                     <li> <a
+                                             href="./routes/product.html">
+                                             Tire Warranties</a></li>
+                                 </ul>
+                             </div>
+                             <div class="second div">
+                               <div>
+                                 <a href="../routes/product.html">
+                                   <img src="../resources/navtire.png" alt="">
+                                 </a>
+                               </div>
+                                   
+                             </div>
+                             
+                             
+                         </div>
+                     </div>
+                 </li>
+                 <li><a href="">AUTO REPAIR</a>
+                     <div class="mega-menu megamenutire">
+                         <div class="mega-menu-items ">
+                           <div class="mega-menu-uls">
+                             <a href="../routes/product.html">
+                               
+                               <img src="../resources/dp2.png" alt="">
+                             </a>
+                             </div>
+                         </div>
+                     </div>
+                 </li>
+                 <li><a href="">AUTO MAINTENANCE</a>
+                   <div class="mega-menu megamenutire">
+                     <div class="mega-menu-items ">
+                       <div class="mega-menu-uls">
+                         <a href="../routes/product.html">
 
+                           <img src="../resources/dp3.png" alt="">
+                         </a>
+                         </div>
+                     </div>
+                 </div>
+                 </li>
+                 <li><a href="">COUPONS</a></li>
+                 <li><a target="_blank" href="#">FIND A STORE</a></li>
+             </ul>
+         </div>
+         <div></div>
+         <div class="nav-right">
+             <div>
+                <img alt="cart" style="width: 50%;" src="https://img.icons8.com/cotton/64/null/shopping-cart--v1.png"/>
+                
+               </div>
+               
+               <div id="login">
+                 <input style="padding: 5px; width: 120%;" type="number" placeholder="PIN CODE"> 
+               </div>
 
-                                    <li><a href="#"> Recurring Payments</a></li>
-                                    <li><a href="#"> Overview</a></li>
-                                    <li><a href="#"> Multiple Payment Methods</a></li>
-                                    <li><a href="#"> Multiple Payment Gateways</a></li>
-                                    <li><a href="#"> Smart Dunning</a></li>
-                                    <li><a href="#"> Chargeback Management</a></li>
-                                    <li><a href="#"> Checkout Experience</a></li>
-                                    <li><a href="#"> Receivables</a></li>
-                                </ul>
-                            </div>
-                            <div>
-
-                                <h4>Accounting & Taxes
-                                    Overview ></h4>
-                                <ul>
-
-                                    <li><a href="#"> Revenue Recognition</a></li>
-                                    <li><a href="#"> Manage Sales Tax</a></li>
-                                    <li><a href="#"> One-Click Reconciliation</a></li>
-
-                                </ul>
-                                <h4>Revenue Recognition></h4>
-                                <ul>
-
-                                    <li><a href="#"> Manage Sales Tax</a></li>
-                                    <li><a href="#"> One-Click Reconciliation</a></li>
-
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li><a target="_blank" href="https://www.chargebee.com/pricing/?ref=navbar">Pricing</a></li>
-                <li><a href="">Solutions</a>
-                    <div class="mega-menu">
-                        <div class="mega-menu-items">
-                            <div id="mega-menu-uls">
-                                <h4>BY STRATEGY ></h4>
-                                <ul>
-
-                                    <li><a
-                                            href="https://www.chargebee.com/solutions/rapidly-iterate-pricing/?ref=navbar">
-                                            Roll Out New Pricing</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/solutions/rapidly-iterate-pricing/?ref=navbar">
-                                            Expand Globally</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/solutions/rapidly-iterate-pricing/?ref=navbar">
-                                            Move Upmarket</a></li>
-                                    <li><a
-                                            href="https://www.chargebee.com/solutions/rapidly-iterate-pricing/?ref=navbar">
-                                            Shift to Subscriptions</a></li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h4> BY ROLE ></h4>
-                                <ul>
-
-
-
-                                    <li><a href="#"> Finance</a></li>
-                                    <li><a href="#"> Sales</a></li>
-                                    <li><a href="#"> RevOps</a></li>
-                                </ul>
-                            </div>
-                            <div>
-
-                                <h4> BY BUSINESS MODEL ></h4>
-                                <ul>
-
-                                    <li><a href="#"> Self-serve</a></li>
-                                    <li><a href="#"> Sales-driven</a></li>
-
-                                </ul>
-
-                            </div>
-                            <div>
-
-                                <h4> By Business Size ></h4>
-                                <ul>
-
-                                    <li><a href="#"> Startup</a></li>
-                                    <li><a href="#"> ScaleUp</a></li>
-                                    <li><a href="#"> Enterprise</a></li>
-                                </ul>
-
-                            </div>
-                            <div>
-
-                                <h4> By Geography ></h4>
-                                <ul>
-
-
-                                    <li><a href="#"> Europe</a></li>
-                                    <li><a href="#"> Australia</a></li>
-                                    <li><a href="#"> India</a></li>
-                                </ul>
-
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li><a href="">Cusomers</a>
-
-                </li>
-                <li><a href="">Resources</a>
-
-                    <div class="mega-menu">
-                        <div class="mega-menu-items" id="resources-grid">
-
-
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                            <div>
-                                <p>A quick overview of Chargebee to help you understand and implement it.
-                                    Chargebee Docs</p>
-                                <a href="#"> Learn More</a>
-                            </div>
-                        </div>
-
-                </li>
-                <li><a target="_blank" href="https://www.chargebee.com/partners/">Partners</a></li>
-            </ul>
-        </div>
-        <div></div>
-        <div class="nav-right">
-            <div>
-                <lord-icon src="https://cdn.lordicon.com/oqhlhtfq.json" trigger="hover"
-                    style="width:30px;height:30px">
-                </lord-icon>
-                <p>English</p>
-            </div>
-
-            <div id="login">
-                <lord-icon src="https://cdn.lordicon.com/ajkxzzfb.json" trigger="hover"
-                    style="width:30px;height:30px">
-                </lord-icon>
-                <a class="login-a" href="login.html">login ></a>
-            </div>
-            <div>
-                <div class="demo">
-                    <p>Get a Demo > </p>
-                </div>
-            </div>
-        </div>
-    </nav>
-</header>`
+               <div>
+                 <img style="width: 50%;" src="https://img.icons8.com/cotton/64/null/search--v1.png"/>
+             </div>
+         </div>
+     </nav>
+ </header>
+   <!-- logo -->
+   <!-- tire -->
+   <!-- auto repair -->
+   <!-- auto maintenance -->
+   <!-- coupons -->
+   <!-- find a store -->
+   <!-- cart logo -->
+   <!-- serach city -->
+ </div>
+</div>`
 
 }
 
